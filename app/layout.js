621571ee7e352b850e26ef648a4a32bffca95a6b
Trehub Anatolii <tridfoto@gmail.com>
2023-06-02 12:13:14 +0300

    delete inter in layout

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import { Header } from "@/components/Header";
 // import { Inter } from "next/font/google";
 import { Footer } from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "WebStudio",
